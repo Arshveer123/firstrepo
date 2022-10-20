8df7350b95fd8d1a9bc77ca88750c41d9f91f4da
Arshveer123 <deolarshveer987@gmail.com>
2022-10-20 09:41:30 -0700

    ---hi there!

---
+++ src/test/java/java1/hello.java
@@ -0,0 +1,11 @@
+package java1;
+
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("hi..this is my first git repo");
+		System.out.println("HI..THIS IS MY FIRST COMMIT");
+		
+		
+
+	}
+}
